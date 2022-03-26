0d0bffe8431db7145d2533871a32923f29d4ed98
SamanjaCartagena <cart.samanja09@gmail.com>
2022-03-26 00:18:51 -0400

    boiler plate code next node middleware

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req,res) => {
+app.use(logger)
+
+app.get('/',  (req,res) => {
+    console.log('Home')
     res.send('Home Page')
 })
 
-app.get('/users', (req,res) => {
+app.get('/users', auth, (req,res) => {
+    console.log(`User is admin =${req.admin}`)
+    console.log('Users Page')
     res.send('Users Page')
+})
+app.get('/blog', (req,res) => {
+    console.log('bloggers Page')
+    res.send('bloggers Page')
 })
 function logger(req, res, next){
     console.log('Log')
+    next()
+}
+
+function auth(req,res, next) {
+    if(req.query.admin ==='true'){
+        req.admin = true
+        next()
+    }else{
+        res.send('No auth')
+    }
+    console.log('Auth')
+    next()
 }
 
 app.listen(4000);
